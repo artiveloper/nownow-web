e4316cdb93cb930ab2b1e598844b98dd347fadd6
RAVIKIM <artiveloper@gmail.com>
2020-01-08 23:47:15 +0900

    first commit

---
+++ src/main/java/kr/nownow/NownowWebApplication.java
@@ -0,0 +1,13 @@
+package kr.nownow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NownowWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NownowWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/nownow/NownowWebApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.nownow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NownowWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
