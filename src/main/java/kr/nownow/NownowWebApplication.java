28f49d07c4aebfea6718ffd3cedb4ecae1ccaca3
ravi <artiveloper@gmail.com>
2020-01-20 14:43:20 +0900

    update env configuration

--- src/main/java/kr/nownow/NownowWebApplication.java
+++ src/main/java/kr/nownow/NownowWebApplication.java
@@ -2,12 +2,19 @@ package kr.nownow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class NownowWebApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NownowWebApplication.class, args);
-	}
+    public static final String CONFIG_LOCATION = "spring.config.location=" +
+            "classpath:application.yml" +
+            "/home/nownow-web/config/real-application.yml";
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(NownowWebApplication.class)
+                .properties(CONFIG_LOCATION)
+                .run(args);
+    }
 
 }
